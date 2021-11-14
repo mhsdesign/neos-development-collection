4cc7355ab4aabc33b29d0bb1037b053e86a3ae99
mhsdesign <85400359+mhsdesign@users.noreply.github.com>
2021-11-14 12:33:23 +0100

    TASK: Fusion remove namespace alias declaration Removes the ability to declare alias namespaces and to use the Neos.Fusion default namespace. Adjusted tests in the Neos.Fusion Package for this new behavior.

--- Neos.Fusion/Classes/Core/ParserInterface.php
+++ Neos.Fusion/Classes/Core/ParserInterface.php
@@ -31,25 +31,4 @@ interface ParserInterface
      * @api
      */
     public function parse($sourceCode, $contextPathAndFilename = null, array $objectTreeUntilNow = []);
-
-    /**
-     * Sets the given alias to the specified namespace.
-     *
-     * The namespaces defined through this setter or through a "namespace" declaration
-     * in one of the Fusions are used to resolve a fully qualified Fusion
-     * object name while parsing Fusion code.
-     *
-     * The alias is the handle by wich the namespace can be referred to.
-     * The namespace is, by convention, a package key which must correspond to a
-     * namespace used in the prototype definitions for Fusion object types.
-     *
-     * The special alias "default" is used as a fallback for resolution of unqualified
-     * Fusion object types.
-     *
-     * @param string $alias An alias for the given namespace, for example "neos"
-     * @param string $namespace The namespace, for example "Neos.Neos"
-     * @return void
-     * @api
-     */
-    public function setObjectTypeNamespace($alias, $namespace);
 }
